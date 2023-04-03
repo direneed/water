867b8ef825c77a99142f2f71747dbb91117594e5
direneed <129484599+direneed@users.noreply.github.com>
2023-04-03 16:31:19 +0530

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-20.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/trying122/local_manifest.git --depth 1 -b lin1 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
